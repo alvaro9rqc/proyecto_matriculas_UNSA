31275f84117566ff19ccc707aafeb5a743baa610
gustadev24 <lgsc21211@gmail.com>
2025-05-27 17:15:38 -0500

    feat(landing): create polymorphic react button

---
+++ frontend/src/modules/landing/ui/Button.tsx
@@ -0,0 +1,24 @@
+import { cn } from '@/modules/core/lib/utils';
+import type { ComponentPropsWithRef } from 'react';
+import type { VariantProps } from 'tailwind-variants';
+import { buttonVariants, type SupportedTags } from '../lib/buttonUtils';
+
+type ButtonProps<Tag extends SupportedTags> = {
+  as?: Tag;
+} & VariantProps<typeof buttonVariants> &
+  ComponentPropsWithRef<Tag>;
+export default function Button<T extends SupportedTags = 'button'>({
+  as = 'button' as T,
+  variant,
+  size,
+  children,
+  className,
+  ...rest
+}: ButtonProps<T>) {
+  const Tag = as;
+  return (
+    <Tag className={cn(buttonVariants({ variant, size }), className)} {...rest}>
+      {children}
+    </Tag>
+  );
+}
