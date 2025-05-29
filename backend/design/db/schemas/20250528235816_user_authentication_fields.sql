df954956925feea3c3a41ad25e85434bd54dae60
alvaro9rqc <aquispecondo@unsa.edu.pe>
2025-05-28 19:54:40 -0500

    feat(oauth): done migrations to include tokens and correct fields

---
+++ backend/design/db/schemas/20250528235816_user_authentication_fields.sql
@@ -0,0 +1,33 @@
+-- +goose Up
+-- +goose StatementBegin
+/* 
+  1. Add new fields for oauth and mistake with attendee_group_id
+  2. Add foreing key to a provider id
+  Add the attendee_group_id to correct table
+*/
+ALTER TABLE account_user 
+DROP CONSTRAINT IF EXISTS account_user_attendee_group_id_fkey,
+DROP COLUMN IF EXISTS attendee_group_id,
+ADD COLUMN provider_user_id VARCHAR(255),
+ADD COLUMN profile_picture TEXT,
+ADD COLUMN oauth_provider_id SMALLINT;
+
+ALTER TABLE account_user
+ADD CONSTRAINT account_user_oauth_provider_id_fkey
+FOREIGN KEY (oauth_provider_id) 
+REFERENCES oauth_provider(id)
+ON DELETE SET NULL;
+
+ALTER TABLE attendee 
+ADD COLUMN attendee_group_id SMALLINT NOT NULL;
+
+ALTER TABLE attendee 
+ADD CONSTRAINT attendee_attendee_group_id_fkey
+FOREIGN KEY(attendee_group_id) 
+REFERENCES attendee_group(id);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+
+-- +goose StatementEnd
