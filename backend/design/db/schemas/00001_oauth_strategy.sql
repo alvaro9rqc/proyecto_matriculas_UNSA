f7cea07800a12a7e049649f38adbfa83978a2e75
ynoacamino <ynoacamino@gmail.com>
2025-06-03 17:17:57 -0500

    fix(design): fix order of migations and remove tables

---
+++ backend/design/db/schemas/00001_oauth_strategy.sql
@@ -0,0 +1,40 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE oauth_provider (
+    id SMALLSERIAL PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
+);
+
+CREATE TABLE account (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    name VARCHAR(50) NOT NULL,
+    surname VARCHAR(50),
+    avatar_url TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    oauth_provider_id SMALLINT NULL,
+    access_token TEXT,
+    refresh_token TEXT,
+    FOREIGN KEY (oauth_provider_id) REFERENCES oauth_provider (id) ON DELETE SET NULL
+);
+
+CREATE TABLE account_session (
+    id SERIAL PRIMARY KEY,
+    token TEXT,
+    expiration_date TIMESTAMPTZ,
+    user_agent TEXT,
+    ip_address TEXT,
+    account_id INTEGER,
+    FOREIGN KEY (account_id) REFERENCES account (id) ON DELETE CASCADE
+);
+
+-- +goose StatementEnd
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS account_session;
+
+DROP TABLE IF EXISTS account;
+
+DROP TABLE IF EXISTS oauth_provider;
+
+-- +goose StatementEnd
