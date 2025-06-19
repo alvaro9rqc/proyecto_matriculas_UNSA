6890b2c8e8611a1ea402ac5280362c309cd5a39b
Yenaro Joel Noa Camino <ynoacamino@gmail.com>
2025-06-18 21:43:53 -0500

    feat: add develop enviroment

--- backend/config/config.go
+++ backend/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -34,7 +35,15 @@ type Config struct {
 }
 
 func NewConfig() (*Config, error) {
-	err := godotenv.Load()
+	devMode := flag.Bool("development", false, "Usar configuración de desarrollo")
+	flag.Parse()
+
+	envFile := ".env"
+	if *devMode {
+		envFile = ".env.development"
+	}
+
+	err := godotenv.Load(envFile)
 	if err != nil {
 		fmt.Println("Error loading .env file")
 	}
