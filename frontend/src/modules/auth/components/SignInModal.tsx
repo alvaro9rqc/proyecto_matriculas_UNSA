49613bf20de2f1e8c374c554dccc2c69ba3578a5
gustadev24 <lgsc21211@gmail.com>
2025-06-19 17:31:56 -0500

    refactor(modal): use shadcn modal

--- frontend/src/modules/auth/components/SignInModal.tsx
+++ frontend/src/modules/auth/components/SignInModal.tsx
@@ -2,24 +2,32 @@ import GoogleLogo from '@/assets/google.svg?react';
 import Logo from '@/assets/logo.svg?react';
 import MicrosoftLogo from '@/assets/microsoft.svg?react';
 import { Button } from '@/modules/core/ui/button';
-import * as Dialog from '@radix-ui/react-dialog';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/modules/core/ui/dialog';
 
 export default function SignInModal() {
   return (
-    <Dialog.Root>
-      <Dialog.Trigger asChild>
+    <Dialog>
+      <DialogTrigger asChild>
         <Button>Iniciar sesión</Button>
-      </Dialog.Trigger>
-      <Dialog.Portal>
-        <Dialog.Overlay className="fixed inset-0 bg-black/25 animate-fade-in" />
-        <Dialog.Content className="bg-white outline-[1px] outline-gray-200 rounded-lg shadow-lg fixed top-1/2 left-1/2 -translate-1/2 p-4 md:p-8 flex flex-col gap-y-4 items-center w-[90vw] max-w-xl focus:outline-none animate-content-show">
-          <Dialog.Title className="inline-flex items-center gap-x-2">
+      </DialogTrigger>
+      <DialogContent className="p-10 gap-y-14">
+        <DialogHeader className="flex flex-col items-center gap-y-4">
+          <DialogTitle className="inline-flex items-center gap-x-2">
             <Logo className="h-8" />
             <span className="text-2xl font-semibold">Tuitions</span>
-          </Dialog.Title>
-          <Dialog.Description className="text-center text-balance text-md mb-8">
+          </DialogTitle>
+          <DialogDescription>
             Ingresa a tu cuenta y revisa tus matrículas
-          </Dialog.Description>
+          </DialogDescription>
+        </DialogHeader>
+        <div className="flex flex-col gap-y-4">
           <Button
             variant="secondary"
             className="inline-flex justify-center items-center gap-x-2 w-full"
@@ -34,16 +42,8 @@ export default function SignInModal() {
             <MicrosoftLogo className="size-4" />
             Microsoft
           </Button>
-          <Dialog.Close asChild>
-            <Button
-              variant="ghost"
-              className="absolute top-2 right-2 md:top-4 md:right-4"
-            >
-              X
-            </Button>
-          </Dialog.Close>
-        </Dialog.Content>
-      </Dialog.Portal>
-    </Dialog.Root>
+        </div>
+      </DialogContent>
+    </Dialog>
   );
 }
