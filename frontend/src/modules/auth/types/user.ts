60b52ba09f81ca5d3d59d741584e0632368e2062
gustadev24 <lgsc21211@gmail.com>
2025-06-23 16:18:07 -0500

    chore(auth): update user types

--- frontend/src/modules/auth/types/user.ts
+++ frontend/src/modules/auth/types/user.ts
@@ -1,7 +1,21 @@
+import type { Roles } from '@/modules/auth/lib/roles';
+
 export interface User {
   id: string;
   email: string;
-  firstNames: string;
-  lastNames: string;
-  remainingNameS: string;
+  name: string;
+  surname: string;
+  avatar_url?: string;
 }
+
+export type Admin = User & {
+  role: typeof Roles.ADMIN;
+};
+
+export type Student = User & {
+  role: typeof Roles.STUDENT;
+};
+
+export type AuthRequired<U extends User = User> = {
+  user: U;
+};
