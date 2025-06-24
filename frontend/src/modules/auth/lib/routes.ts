06c71488e04a4ccfbe186e6100529adde9e3da7a
gustadev24 <lgsc21211@gmail.com>
2025-06-24 13:35:58 -0500

    chore: update login route constant

--- frontend/src/modules/auth/lib/routes.ts
+++ frontend/src/modules/auth/lib/routes.ts
@@ -1,4 +1,3 @@
 import { getRouteFromPath } from '@/modules/core/lib/routes';
-import type { Route } from '@/modules/core/types/route';
 
-export const LOGIN_ROUTE = getRouteFromPath('/auth/login') || ({} as Route);
+export const LOGIN_ROUTE = getRouteFromPath('/auth/login');
