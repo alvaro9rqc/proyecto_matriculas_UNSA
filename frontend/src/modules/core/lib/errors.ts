4b945e0c2d2bdc9e27425075f86403e19c7238ae
gustadev24 <lgsc21211@gmail.com>
2025-06-24 08:42:55 -0500

    feat: create constant errors

--- frontend/src/modules/core/lib/errors.ts
+++ frontend/src/modules/core/lib/errors.ts
@@ -4,3 +4,13 @@ export const INTERNAL_SERVER_ERROR: ErrorResponse = {
   message: 'An error occurred while processing your request.',
   status: 500,
 };
+
+export const NOT_FOUND_ERROR: ErrorResponse = {
+  message: 'The requested resource was not found.',
+  status: 404,
+};
+
+export const UNAUTHORIZED_ERROR: ErrorResponse = {
+  message: 'You are not authorized to access this resource.',
+  status: 401,
+};
