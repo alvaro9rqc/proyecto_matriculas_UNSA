c4176149e934a0db2315be66d0de337d3b7ab6ca
gustadev24 <lgsc21211@gmail.com>
2025-06-25 07:44:57 -0500

    fix(actions): use authService on user actions

--- frontend/src/modules/auth/lib/actions.ts
+++ frontend/src/modules/auth/lib/actions.ts
@@ -1,6 +1,8 @@
 import { defineAction } from 'astro:actions';
-import { authApi } from '@/modules/auth/lib/api';
+import { authService } from '@/modules/auth/services';
 
 export const user = {
-  getUser: defineAction({ handler: () => authApi.getUser() }),
+  getUser: defineAction({
+    handler: (_, context) => authService(context.cookies).getUser(),
+  }),
 };
