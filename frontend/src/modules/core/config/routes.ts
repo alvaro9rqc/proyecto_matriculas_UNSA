c5029044da8f4f8a07eca4f45c6c416f862108bb
gustadev24 <lgsc21211@gmail.com>
2025-06-25 07:24:25 -0500

    chore: set fullPath on every route on routes configuration

--- frontend/src/modules/core/config/routes.ts
+++ frontend/src/modules/core/config/routes.ts
@@ -10,44 +10,53 @@ export const routesConfig: Route = {
       name: 'not-found',
       title: 'Página no encontrada',
       path: 'not-found',
+      fullPath: '/not-found',
     },
     {
       name: 'unauthorized',
       title: 'No autorizado',
       path: 'unauthorized',
+      fullPath: '/unauthorized',
     },
     {
       name: 'dashboard',
       title: 'Panel de control',
       path: 'dashboard',
       icon: House,
+      fullPath: '/dashboard',
       sub: [
         {
           name: 'dashboard-admin',
           title: 'Panel de administración',
           path: 'admin',
+          fullPath: '/dashboard/admin',
           sub: [
             {
               name: 'dashboard-admin-instituciones',
               title: 'Instituciones - Panel de administración',
               path: 'instituciones',
+              fullPath: '/dashboard/admin/instituciones',
               sub: [
                 {
                   name: 'dashboard-admin-instituciones-crear',
                   title: 'Crear institución - Panel de administración',
                   path: 'crear',
+                  fullPath: '/dashboard/admin/instituciones/crear',
                   sub: [
                     {
                       name: 'dashboard-admin-instituciones-crear-bienvenido',
                       title:
                         'Bienvenido - Crear institución - Panel de administración',
                       path: 'bienvenido',
+                      fullPath:
+                        '/dashboard/admin/instituciones/crear/bienvenido',
                     },
                     {
                       name: 'dashboard-admin-instituciones-crear-nombre',
                       title:
                         'Nombre - Crear institución - Panel de administración',
                       path: 'nombre',
+                      fullPath: '/dashboard/admin/instituciones/crear/nombre',
                     },
                   ],
                 },
@@ -56,11 +65,14 @@ export const routesConfig: Route = {
                   title: 'Institución - Panel de administración',
                   dynamic: true,
                   param: 'institucion_id',
+                  fullPath: '/dashboard/admin/instituciones/:institucion_id',
                   sub: [
                     {
                       name: 'dashboard-admin-instituciones-one-cursos',
                       title: 'Cursos - Institución - Panel de administración',
                       path: 'cursos',
+                      fullPath:
+                        '/dashboard/admin/instituciones/:institucion_id/cursos',
                       sub: [
                         {
                           name: 'dashboard-admin-instituciones-one-cursos-one',
@@ -68,6 +80,8 @@ export const routesConfig: Route = {
                             'Curso - Institución - Panel de administración',
                           dynamic: true,
                           param: 'curso_id',
+                          fullPath:
+                            '/dashboard/admin/instituciones/:institucion_id/cursos/:curso_id',
                         },
                       ],
                     },
@@ -76,6 +90,8 @@ export const routesConfig: Route = {
                       title:
                         'Estudiantes - Institución - Panel de administración',
                       path: 'estudiantes',
+                      fullPath:
+                        '/dashboard/admin/instituciones/:institucion_id/estudiantes',
                       sub: [
                         {
                           name: 'dashboard-admin-instituciones-one-estudiantes-one',
@@ -83,6 +99,8 @@ export const routesConfig: Route = {
                             'Estudiante - Institución - Panel de administración',
                           dynamic: true,
                           param: 'estudiante_id',
+                          fullPath:
+                            '/dashboard/admin/instituciones/:institucion_id/estudiantes/:estudiante_id',
                         },
                       ],
                     },
@@ -91,30 +109,40 @@ export const routesConfig: Route = {
                       title:
                         'Matrículas - Institución - Panel de administración',
                       path: 'matriculas',
+                      fullPath:
+                        '/dashboard/admin/instituciones/:institucion_id/matriculas',
                       sub: [
                         {
                           name: 'dashboard-admin-instituciones-one-matriculas-crear',
                           title:
                             'Crear matrícula - Institución - Panel de administración',
                           path: 'crear',
+                          fullPath:
+                            '/dashboard/admin/instituciones/:institucion_id/matriculas/crear',
                           sub: [
                             {
                               name: 'dashboard-admin-instituciones-one-matriculas-crear-inicio',
                               title:
                                 'Inicio - Crear matrícula - Institución - Panel de administración',
                               path: 'inicio',
+                              fullPath:
+                                '/dashboard/admin/instituciones/:institucion_id/matriculas/crear/inicio',
                             },
                             {
                               name: 'dashboard-admin-instituciones-one-matriculas-crear-subirDatos',
                               title:
                                 'Subir Datos - Crear matrícula - Institución - Panel de administración',
                               path: 'subir-datos',
+                              fullPath:
+                                '/dashboard/admin/instituciones/:institucion_id/matriculas/crear/subir-datos',
                             },
                             {
                               name: 'dashboard-admin-instituciones-one-matriculas-crear-listos',
                               title:
                                 'Todo listo - Crear matrícula - Institución - Panel de administración',
                               path: 'listos',
+                              fullPath:
+                                '/dashboard/admin/instituciones/:institucion_id/matriculas/crear/listos',
                             },
                           ],
                         },
@@ -124,12 +152,16 @@ export const routesConfig: Route = {
                             'Matrícula - Institución - Panel de administración',
                           dynamic: true,
                           param: 'matricula_id',
+                          fullPath:
+                            '/dashboard/admin/instituciones/:institucion_id/matriculas/:matricula_id',
                           sub: [
                             {
                               name: 'dashboard-admin-instituciones-one-matriculas-one-horarios',
                               title:
                                 'Horarios - Matrícula - Institución - Panel de administración',
                               path: 'horarios',
+                              fullPath:
+                                '/dashboard/admin/instituciones/:institucion_id/matriculas/:matricula_id/horarios',
                             },
                           ],
                         },
@@ -145,28 +177,36 @@ export const routesConfig: Route = {
           name: 'dashboard-estudiante',
           title: 'Panel de estudiante',
           path: 'estudiante',
+          fullPath: '/dashboard/estudiante',
           sub: [
             {
               name: 'dashboard-estudiante-instituciones',
               title: 'Instituciones - Panel de estudiante',
               path: 'instituciones',
+              fullPath: '/dashboard/estudiante/instituciones',
               sub: [
                 {
                   name: 'dashboard-estudiante-instituciones-one',
                   title: 'Institución - Panel de estudiante',
                   dynamic: true,
                   param: 'institucion_id',
+                  fullPath:
+                    '/dashboard/estudiante/instituciones/:institucion_id',
                   sub: [
                     {
                       name: 'dashboard-estudiante-instituciones-one-cursos',
                       title: 'Cursos - Institución - Panel de estudiante',
                       path: 'cursos',
+                      fullPath:
+                        '/dashboard/estudiante/instituciones/:institucion_id/cursos',
                       sub: [
                         {
                           name: 'dashboard-estudiante-instituciones-one-cursos-one',
                           title: 'Curso - Institución - Panel de estudiante',
                           dynamic: true,
                           param: 'curso_id',
+                          fullPath:
+                            '/dashboard/estudiante/instituciones/:institucion_id/cursos/:curso_id',
                         },
                       ],
                     },
@@ -174,6 +214,8 @@ export const routesConfig: Route = {
                       name: 'dashboard-estudiante-instituciones-one-matriculas',
                       title: 'Matrículas - Institución - Panel de estudiante',
                       path: 'matriculas',
+                      fullPath:
+                        '/dashboard/estudiante/instituciones/:institucion_id/matriculas',
                       sub: [
                         {
                           name: 'dashboard-estudiante-instituciones-one-matriculas-one',
@@ -181,18 +223,24 @@ export const routesConfig: Route = {
                             'Matrícula - Institución - Panel de estudiante',
                           dynamic: true,
                           param: 'matricula_id',
+                          fullPath:
+                            '/dashboard/estudiante/instituciones/:institucion_id/matriculas/:matricula_id',
                           sub: [
                             {
                               name: 'dashboard-estudiante-instituciones-one-matriculas-one-constancia',
                               title:
                                 'Constancia - Matrícula - Institución - Panel de estudiante',
                               path: 'constancia',
+                              fullPath:
+                                '/dashboard/estudiante/instituciones/:institucion_id/matriculas/:matricula_id/constancia',
                             },
                             {
                               name: 'dashboard-estudiante-instituciones-one-matriculas-one-horarios',
                               title:
                                 'Horarios - Matrícula - Institución - Panel de estudiante',
                               path: 'horarios',
+                              fullPath:
+                                '/dashboard/estudiante/instituciones/:institucion_id/matriculas/:matricula_id/horarios',
                             },
                           ],
                         },
