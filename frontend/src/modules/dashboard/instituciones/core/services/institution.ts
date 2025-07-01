ee18c31af9bad4e007d9ab61292551be8f0f8e0f
gustadev24 <lgsc21211@gmail.com>
2025-07-01 13:04:42 -0500

    fix: set cookie on institutions request

--- frontend/src/modules/dashboard/instituciones/core/services/institution.ts
+++ frontend/src/modules/dashboard/instituciones/core/services/institution.ts
@@ -1,6 +1,8 @@
+import { authService } from '@/modules/auth/core/services/auth';
 import { INTERNAL_SERVER_ERROR } from '@/modules/core/lib/errors';
 import type { ApiResponse } from '@/modules/core/types/api';
 import type { Institution } from '@/modules/dashboard/instituciones/core/types/institution';
+import type { AstroCookies } from 'astro';
 import { BACKEND_URL } from 'astro:env/client';
 
 class InstitutionService {
@@ -10,13 +12,26 @@ class InstitutionService {
     this.apiInstitutionUrl = `${BACKEND_URL}/institutions`;
   }
 
-  async getInstitutions(): ApiResponse<Institution[]> {
+  async getInstitutions(cookies: AstroCookies): ApiResponse<Institution[]> {
+    const { data: sessionToken, error } =
+      await authService.validateSessionToken(cookies);
+
+    if (error) {
+      return {
+        data: undefined,
+        error: error,
+      };
+    }
+
     try {
       const response = await fetch(
         // TODO: Change real API URL
         this.apiInstitutionUrl,
         {
           method: 'GET',
+          headers: {
+            Cookie: `session_token=${sessionToken}`,
+          },
         },
       );
 
