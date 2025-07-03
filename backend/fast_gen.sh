85ec5c902d22152c1312725598d09d7d7de1137e
Yenaro Noa Camino <ynoacamino@gmail.com>
2025-07-02 19:27:18 -0500

    fix: fix build errors in Dockerfile

--- backend/fast_gen.sh
+++ backend/fast_gen.sh
@@ -1,12 +1,10 @@
-rm -rf gen
+rm -rf ./gen
 
 #go mod tidy # escanea tu projecto y actualiza tu go.mod
 goa gen github.com/enrollment/design/api
 sqlc generate
 
 if [ "$?" -ne "0" ]
-then 
+then
     echo "Code generation completed successfully."
 fi
-
-
