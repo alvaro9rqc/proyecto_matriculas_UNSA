4086a34cf9d98ea9a3df053b890a77122bc9c13d
alvaro9rqc <aquispecondo@unsa.edu.pe>
2025-07-03 05:07:28 -0500

    feat(student_port): add new Method designed in sql

--- backend/internal/ports/student_port.go
+++ backend/internal/ports/student_port.go
@@ -10,4 +10,5 @@ type StudentRepositoryInterface interface {
 	CreateStudent(ctx context.Context, arg db.CreateStudentParams) error
 	FullListStudents(ctx context.Context) ([]db.FullListStudentsRow, error)
 	ListStudents(ctx context.Context, arg db.ListStudentsParams) ([]db.ListStudentsRow, error)
+	GetStudentIdByToken(ctx context.Context, token string) (int32, error)
 }
